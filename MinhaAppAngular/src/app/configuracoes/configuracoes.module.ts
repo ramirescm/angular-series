da44e9d201d2ae08b9913a1a32265afdef6f94ed
Ramires Marques <ramirescm@gmail.com>
2020-08-22 13:41:12 -0300

    inicio estudos

---
+++ MinhaAppAngular/src/app/configuracoes/configuracoes.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// criando um novo modulo
+
+@NgModule({
+  declarations: [
+    // meus componentes
+  ],
+  imports: [
+    CommonModule
+    // meus modulos
+  ],
+  providers: [
+    // meus servicos    
+  ]
+})
+export class ConfiguracoesModule {
+
+
+
+}
